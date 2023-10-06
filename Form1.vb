35e7ea47e7b29e1ae68efbf5191c7ccf57d71fdd
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-06 07:20:23 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+
+        n = TextBox2.Text
+
+        TextBox3.Text = Math.Round(Val(TextBox1.Text), n)
+    End Sub
+End Class
